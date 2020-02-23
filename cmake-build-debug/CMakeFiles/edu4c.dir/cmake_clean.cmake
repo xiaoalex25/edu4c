87996d5a0e790daefcdb50fb1e3a523964f423ad
xiaoalex25 <1136788012@qq.com>
2020-02-23 23:36:38 +0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/edu4c.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/edu4c.dir/main.c.obj"
+  "edu4c.exe"
+  "edu4c.exe.manifest"
+  "edu4c.pdb"
+  "libedu4c.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/edu4c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
